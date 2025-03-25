cd77d3a627bdd60e21fdee9c144bb376f758ef0f
BETAKOTLETA <viktor.oleniuk@gmail.com>
2025-03-25 16:21:46 +0200

    Done

---
+++ Psotify/Psotify.DataModel/Migrations/20250325133354_CorrectSpellOfLength.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Psotify.DataModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrectSpellOfLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Lenght",
+                table: "Songs",
+                newName: "Length");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Length",
+                table: "Songs",
+                newName: "Lenght");
+        }
+    }
+}
---
+++ Psotify/Psotify.DataModel/PsotifyDbContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Psotify.DataModel
+{
+    public class PsotifyDbContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+
+        public DbSet<Artist> Artists { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Psotify;Trusted_Connection=True;");
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Song>().HasData(
+        //        new Song
+        //        {
+        //            SongId = 1,
+        //            Title = "Imagine",
+        //            Artist = "John Lennon",
+        //            Length = "03:04"
+        //        },
+        //        new Song
+        //        {
+        //            SongId = 2,
+        //            Title = "Wish You Were Here",
+        //            Artist = "Pink Floyd",
+        //            Length = "05:40"
+        //        }
+        //    );
+        //}
+    }
+
+
+
+    public class Song
+    {
+        public int SongId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [MaxLength(10)]
+        public string Length { get; set; }
+
+        public Artist Artist { get; set; }
+
+        public int ArtistId { get; set; }
+    }
+
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        public List<Song> Songs { get; set; }
+
+    }
+}
